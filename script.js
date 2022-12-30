f88069051bec0ca5be3f82c73d19ba9cc0636284
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-30 12:53:09 +0100

    Calculator: Made the calculator with starting css, started adding functions

---
+++ script.js
@@ -0,0 +1,41 @@
+let result = 0;
+let tempResult = 0;
+
+// add input function for numOne, numTwo.
+function add(numOne, numTwo){
+    let result = numOne + numTwo;
+    return result;
+    console.log(result);
+}
+
+function sub(numOne, numTwo){
+    result = numOne - numTwo; 
+    return result;
+        console.log(result);
+}
+
+function divide(numOne, numTwo){
+    if(numTwo == 0){
+        alert("You cant divide by 0. Try with a different number");
+    } else {
+    result = numOne / numTwo;
+    return result;
+    console.log(result);
+    }
+}
+
+function multiply(numOne, numTwo){
+    result = numOne * numTwo;
+    return result;
+    console.log(result);
+}
+
+function operate(){
+
+}
+
+function displayResult(){
+
+}
+
+
